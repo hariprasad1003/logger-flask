c8ece411776e263d37c1f42524024ff549f0376e
hariprasad1003 <hariprasadgurunathan10@gmail.com>
2021-01-12 09:55:48 +0530

    view logs page added

--- app.py
+++ app.py
@@ -1,3 +1,26 @@
+'''
+author - Hari Prasad
+
+resorces - 
+    https://stackoverflow.com/questions/38055762/how-do-you-save-the-inputs-into-text-file
+    https://www.quora.com/What-is-the-right-way-to-read-a-file-and-display-its-content-on-browser-using-flask
+    https://www.kite.com/python/answers/how-to-erase-the-file-contents-of-a-text-file-in-python
+    
+'''
+
+'''
+    text_file = open('logs.txt', 'r') 
+    show_file = text_file.read()
+    print(show_file)
+    return render_template('show_log.html', text=show_file)
+
+    with open('logs.txt', 'r') as f: 
+        return render_template('show_log.html', text=f.read())
+
+'''
+
+
+
 from flask import Flask, render_template, request, jsonify, url_for, redirect
 
 app = Flask(__name__)
@@ -6,25 +29,40 @@ app = Flask(__name__)
 def login():
     return render_template("index.html")
 
+@app.route('/view_log', methods = ['GET', 'POST'])
+def view_log():
+    with open('logs.txt', 'r') as f: 
+        return render_template('show_log.html', text=f.read())
+
 @app.route('/add_entry', methods = ['GET', 'POST'])
 def add_entry():
-    day = request.form.get('day')
-    date_time = request.form.get('date_time')
-    title = request.form.get('title')
-    things_learned = request.form.get('things_learned')
+
+    day             = request.form.get('day')
+    date_time       = request.form.get('date_time')
+    title           = request.form.get('title')
+    things_learned  = request.form.get('things_learned')
 
     text_file = open("logs.txt", "a")
     text_file.write(f'''
-    ===========================================================
-        Day - {day} \n
-        Date & Time - {date_time} \n
-        Title - {title} \n
-        Things Learned - {things_learned} \n
-    ===========================================================
+=========================================================== \n
+Day - {day} \n
+Date & Time - {date_time} \n
+Title - {title} \n
+Things Learned - \n{things_learned} \n \t
+=========================================================== \n
     ''')
     text_file.close()
 
     return redirect(url_for('login'))
+    
+@app.route('/clear_logs', methods = ['GET', 'POST'])
+def clear_logs():
+    
+    text_file = open("logs.txt","r+")
+    text_file.truncate(0)
+    text_file.close()
+
+    return redirect(url_for('view_log'))
 
 if __name__ == "__main__":
     app.run(debug=True, port=3000)
